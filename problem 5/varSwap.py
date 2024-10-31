2c285b56c47a4d8f1cf2422cd0d08931cefddd26
Bell <jrb5839@utulsa.edu>
2024-10-31 09:02:09 -0500

    Q5

--- problem 5/varSwap.py
+++ problem 5/varSwap.py
@@ -1,6 +1,13 @@
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
    #write your code here
    
+   user_val_vect = [user_val2, user_val1, user_val4, user_val3]
+
+   user_val1 = user_val_vect[0]
+   user_val2 = user_val_vect[1]
+   user_val3 = user_val_vect[2]
+   user_val4 = user_val_vect[3]
+
    return user_val1, user_val2, user_val3, user_val4
 
 if __name__ == '__main__':   
@@ -9,6 +16,14 @@ if __name__ == '__main__':
    user_input3 = int(input())
    user_input4 = int(input())
    #store output for every input here
+
+   swapped_vect = ""
+
+   for i in swap_values(user_input1, user_input2, user_input3, user_input4):
+      swapped_vect = swapped_vect + " " + str(i)
+
+   print(swapped_vect)
+
    #print those output
 
  
